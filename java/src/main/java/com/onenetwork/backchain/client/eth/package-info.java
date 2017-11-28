1b84509c168ba11cba91d77a0942e1ff1664200a
Greg Merrill <gmerrill@onenetwork.com>
2017-11-28 15:11:32 -0600

    javadocs

---
+++ java/src/main/java/com/onenetwork/backchain/client/eth/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Implementation of {@link com.onenetwork.backchain.client.BackchainClient}
+ * based on <a href="https://www.ethereum.org/">Ethereum</a>
+ */
+package com.onenetwork.backchain.client.eth;
---
+++ java/src/main/java/com/onenetwork/backchain/client/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Java client for interacting with One Network's Backchain.
+ * <p>
+ * See the <a href="https://github.com/onenetwork/onechain-back-client">onechain-back-client github project</a>
+ * for more details.
+ * </p>
+ */
+package com.onenetwork.backchain.client;
