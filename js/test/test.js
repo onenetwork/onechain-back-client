4cfbb01fd53704c8b9476f4fbe72a06d26f92e5f
Pramod Singh Rana <pramodoo7rana@gmail.com>
2018-05-10 17:46:27 +0530

    updating the testcase

--- js/test/test.js
+++ js/test/test.js
@@ -8,20 +8,17 @@ function newHash() {
 var sampleHash = newHash();
 var sampleHash1 = newHash();
 
-var bc = oneChain.createContentBcClient({
-    blockchain: 'eth',
-    url: 'http://localhost:8545',
-    contractAddress: "0xc5d4b021858a17828532e484b915149af5e1b138",
-    privateKey: "0x8ad0132f808d0830c533d7673cd689b7fde2d349ff0610e5c04ceb9d6efb4eb1"
-});
-
-var disputeBC = oneChain.createDisputeBcClient({
+var config = {
     blockchain: 'eth',
     url: 'http://localhost:8545',
     contentBackchainContractAddress: "0x4a6886a515a4b800f4591a6d6a60e6004a3645ab",
     disputeBackchainContractAddress: "0x4a6886a515a4b800f4591a6d6a60e6004a3645ab",
     privateKey: "0x8ad0132f808d0830c533d7673cd689b7fde2d349ff0610e5c04ceb9d6efb4eb1"
-});
+};
+
+var bc = oneChain.createContentBcClient(config);
+
+var disputeBC = oneChain.createDisputeBcClient(config);
 
 describe('backchain-client', function() {
 
