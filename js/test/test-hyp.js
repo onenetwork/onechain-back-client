7f72870ab967a3e441282ebc25878ba995be267b
devendrarg <devendrarg@users.noreply.github.com>
2018-07-19 15:40:27 +0530

    JS test should verify that participant can call his methods

--- js/test/test-hyp.js
+++ js/test/test-hyp.js
@@ -6,44 +6,53 @@ function newHash() {
     return "0x" + sha256('' + new Date().getTime() + ' ' + Math.random());
 }
 var sampleHash = newHash();
-var sampleHash1 = newHash();
-var sampleHashString = sampleHash1.substring(2);
 
-var contractConfig = {
+var obc = oneChain.createContentBcClient({
     blockchain: 'hyp',
     url: 'http://localhost:4000',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzAyMTM4MzksInVzZXJuYW1lIjoiT3JjaGVzdHJhdG9yVXNlciIsIm9yZ05hbWUiOiJPcmNoZXN0cmF0b3JPcmciLCJpYXQiOjE1MzAxNzc4Mzl9.h5ARvYV4jLMMQpFJNBvinaU1tD1MkKWiengYzzOG1w8'
-};
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzIwMjY4NTEsInVzZXJuYW1lIjoiT3JjaGVzdHJhdG9yVXNlciIsIm9yZ05hbWUiOiJPcmNoZXN0cmF0b3JPcmciLCJpYXQiOjE1MzE5OTA4NTF9.Ioa1Qb0bGrmLBzl16Ncgu7rb9N3pe7r-p3IBOYbKYa0'
+});
 
-
-var bc = oneChain.createContentBcClient(contractConfig);
+var pbc = oneChain.createContentBcClient({
+    blockchain: 'hyp',
+    url: 'http://localhost:4000',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzIwMjY4NTcsInVzZXJuYW1lIjoiUGFydGljaXBhbnRVc2VyIiwib3JnTmFtZSI6IlBhcnRpY2lwYW50T3JnIiwiaWF0IjoxNTMxOTkwODU3fQ.pz-Wv_94sv1XrZtScUtWbFyhDcW4enwNTKm2K7VnZgo'
+});
 
 describe('contentbackchain-client', function() {
     
-        describe('hyperledger', function() {
+        describe('HyperLedger Fabric', function() {
     
             it('supports all smart contract methods for ContentBackchain', function() {
                 var initialHashCount;
-                bc.hashCount().then((hashCount) => {
-		            initialHashCount = hashCount;
-		            return bc.post(sampleHash);
+                obc.hashCount().then((hashCount) => {
+                    initialHashCount = hashCount;
+                    return obc.post(sampleHash);
                 }).then(() => {
-                    return bc.verify(sampleHash);
+                    return obc.verify(sampleHash);
                 }).then((verified) => {
                     expect(verified).to.be.true;
-                    return bc.hashCount();
+                    return obc.hashCount();
                 }).then((hashCount) => {
                     expect(hashCount).to.equal(initialHashCount + 1);
-                    return bc.verify(newHash());
+                    return obc.verify(newHash());
                 }).then((verified) => {
                     expect(verified).to.be.false;
-                    return bc.hashCount();
-                }).then((hashCount) => {  
-                    expect(hashCount).to.equal(initialHashCount + 1);            
-                    console.log('ContentBackchain all test cases passed :)');
-            	});
-    	
+                    return obc.hashCount();
+                }).then((hashCount) => {
+                    return pbc.verify(sampleHash);
+                }).then((verified) => {
+                    expect(verified).to.be.true;
+                    return pbc.hashCount();
+                }).then((hashCount) => {
+                    expect(hashCount).to.equal(initialHashCount + 1);
+                    return pbc.verify(newHash());
+                }).then((verified) => {
+                    expect(verified).to.be.false;
+                    console.log('ContentBackchain all test cases passed. :)');
+                })
           });
     });
+ 
 });
 
